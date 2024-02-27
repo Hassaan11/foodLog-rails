8e520ad280acec8d2d72b218eff01dfa36192394
Hassaan11 <saad117218@gmail.com>
2024-02-27 18:04:24 +0500

    updated test cases

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,14 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should create entry" do
+    entry = Entry.new(meal_type: "Breakfast", calories: 500, proteins: 50, carbohydrates: 50, fats: 50)
+    assert entry.save
+  end
+  
+  test "should not create entry" do
+    entry = Entry.new(calories: 500, proteins: 50, carbohydrates: 50, fats: 50)
+    assert_not entry.save
+  end
 end
